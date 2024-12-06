4ac83cf97a4c137af64c3ad64e52012baf882020
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-06 18:46:00 -0300

    Modelos listos

--- src/curso/admin.py
+++ src/curso/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Alumno, Comision, Curso
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+
+
+@admin.register(Comision)
+class ComisionAdmin(admin.ModelAdmin):
+    list_display = ("curso__nombre", "numero", "fecha_inicio")
+
+
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ("comision", "dni")
