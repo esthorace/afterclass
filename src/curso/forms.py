dfc294577feab7b5cd864fe4f8db440489b25688
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-12-06 19:27:32 -0300

    Formularios para los modelos

---
+++ src/curso/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import Alumno, Comision, Curso
+
+
+class CursoForm(forms.ModelForm):
+    class Meta:
+        model = Curso
+        fields = "__all__"
+
+
+class ComisionForm(forms.ModelForm):
+    class Meta:
+        model = Comision
+        fields = "__all__"
+        widgets = {
+            "fecha_inicio": forms.DateInput(attrs={"type": "date"}),
+        }
+
+
+class AlumnoForm(forms.ModelForm):
+    class Meta:
+        model = Alumno
+        fields = "__all__"
